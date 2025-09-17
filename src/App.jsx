ecc774b0b9f604702972c6fa06e24a09ab46f007
chida_nzemssing_ariel <chidanzemssing123@gmail.com>
2025-09-17 16:24:16 +0100

    group assignment

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import "./styles/App.css";
 import Eja from './components/Header';
 import Header from "./components/Header.jsx"
+import SavingsBox from './components/SavingsBox.jsx';
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
+      <SavingsBox/>
+      <SavingsBox/>
     </div>
   )
 }
