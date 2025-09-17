02e2bb4e61976f2420d1bf4d4e93ab187eec3ae0
Lewiston <m11lewiston@gmail.com>
2025-09-17 16:35:18 +0100

    footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "./styles/App.css";
 import Eja from './components/Header';
 import Header from "./components/Header.jsx"
+import Footer from './components/Footer.jsx';
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
+      <Footer/>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import '../styles/footer.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='footer'>
+      <div className="top-footer">
+        <img src="/assets/img/logo.svg" alt="" />
+        <div className="resource">
+          <h3>Resource</h3>
+          <div className="list">
+            <p>FAQs</p>
+            <p>Contact Us</p>
+            <p>Blog Post</p>
+            <p>Terms and Conditions</p>
+            <p>Privacy Policy</p>
+            <p>Data Protection</p>
+          </div>
+        </div>
+        <div className="products">
+          <h3>Products</h3>
+          <div className="list">
+            <p>Ejara Wallet</p>
+            <p>Savings box</p>
+            <p>Project savings</p>
+            <p>Invest</p>
+            <p>Ejara For Business</p>
+          </div>
+        </div>
+        <div className='footer-downloads'>
+          <img src="/assets/img/play-store.png" alt="" />
+          <img src="/assets/img/app-store.png" alt="" />
+        </div>
+      </div>
+      <div className="bottom-footer">
+        <p>Copyright &copy; 2025 EJARA</p>
+      </div>
       
     </div>
   )
